b1893be5bcbcd5a0e8f693c8ec9971151a1daf66
Karina Kozarova <karinakozarova1303@gmail.com>
2020-03-01 15:25:27 +0100

    added main for actions

--- WindowsFormsApp1/WindowsFormsApp1/MainForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.Designer.cs
@@ -32,23 +32,29 @@
             this.employeesTab = new System.Windows.Forms.TabPage();
             this.stocksTab = new System.Windows.Forms.TabPage();
             this.departmentsTab = new System.Windows.Forms.TabPage();
-            this.label4 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.employeesTab.SuspendLayout();
             this.stocksTab.SuspendLayout();
             this.departmentsTab.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -64,6 +70,10 @@
             // 
             // employeesTab
             // 
+            this.employeesTab.Controls.Add(this.button6);
+            this.employeesTab.Controls.Add(this.button5);
+            this.employeesTab.Controls.Add(this.button4);
+            this.employeesTab.Controls.Add(this.button1);
             this.employeesTab.Location = new System.Drawing.Point(4, 29);
             this.employeesTab.Name = "employeesTab";
             this.employeesTab.Padding = new System.Windows.Forms.Padding(3);
@@ -74,7 +84,8 @@
             // 
             // stocksTab
             // 
-            this.stocksTab.Controls.Add(this.button1);
+            this.stocksTab.Controls.Add(this.button3);
+            this.stocksTab.Controls.Add(this.button2);
             this.stocksTab.Location = new System.Drawing.Point(4, 29);
             this.stocksTab.Name = "stocksTab";
             this.stocksTab.Padding = new System.Windows.Forms.Padding(3);
@@ -95,49 +106,6 @@
             this.departmentsTab.Text = "Departments";
             this.departmentsTab.UseVisualStyleBackColor = true;
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Montserrat SemiBold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(34, 35);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(248, 44);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "Departments:";
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.LightGray;
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(42, 100);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1044, 114);
-            this.panel1.TabIndex = 13;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 65);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(359, 33);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Some long descriptioon here";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 13);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(248, 33);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Department name";
-            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.LightGray;
@@ -148,6 +116,17 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1044, 114);
             this.panel2.TabIndex = 14;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.editdepartment;
+            this.pictureBox2.Location = new System.Drawing.Point(982, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 45);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // label3
             // 
@@ -171,6 +150,17 @@
             this.label5.TabIndex = 1;
             this.label5.Text = "Some long descriptioon here";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(42, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1044, 114);
+            this.panel1.TabIndex = 13;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
@@ -181,26 +171,97 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
+            // label2
             // 
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.editdepartment;
-            this.pictureBox2.Location = new System.Drawing.Point(982, 22);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(48, 45);
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Department name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Some long descriptioon here";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat SemiBold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(248, 44);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Departments:";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(40, 25);
+            this.button1.Location = new System.Drawing.Point(35, 46);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(1151, 65);
-            this.button1.TabIndex = 26;
+            this.button1.TabIndex = 27;
             this.button1.Text = "Create account";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(46, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(1151, 65);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Create stock";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(46, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(1151, 65);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "View upcoming shelf requests";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(35, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(1151, 65);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "Remove employee from the system";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(35, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(1151, 65);
+            this.button5.TabIndex = 29;
+            this.button5.Text = "Fire employee";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(35, 384);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(1151, 65);
+            this.button6.TabIndex = 30;
+            this.button6.Text = "Search for employee";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -211,15 +272,16 @@
             this.Name = "MainForm";
             this.Text = "Media Bazar";
             this.tabControl1.ResumeLayout(false);
+            this.employeesTab.ResumeLayout(false);
             this.stocksTab.ResumeLayout(false);
             this.departmentsTab.ResumeLayout(false);
             this.departmentsTab.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -240,5 +302,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
     }
 }
