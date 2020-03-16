ce31b03394633b29357e3e0ad3fb1687a0a36ab7
Karina Kozarova <karinakozarova1303@gmail.com>
2020-03-16 19:13:06 +0100

    started adding the login logic

--- WindowsFormsApp1/WindowsFormsApp1/LoginForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,19 +17,39 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
             mediaBazarLogoLogin.BorderStyle = BorderStyle.None;
-
-            // TODO: this is added for debugging now, remove later
-            
-            (new AddStock()).Show();
-            (new SearchForEmployee()).Show();
-            (new RemoveEmployee()).Show();
-            (new ShelfRestockRequests()).Show();
-            (new MainForm()).Show();
-            (new CreateAccount()).Show();
         }
 
         private void loginBttn_Click(object sender, EventArgs e)
         {
+            
+            if(String.IsNullOrWhiteSpace(usernameLoginInput.Text))
+            {
+                MessageBox.Show("enter username");
+                return;
+            }
+            
+            if(String.IsNullOrWhiteSpace(passwordLoginInput.Text))
+            {
+                MessageBox.Show("enter password");
+                return;
+            }
+
+            string username = usernameLoginInput.Text;
+            string psswd = passwordLoginInput.Text;
+            
+            MySqlConnection conn = new MySqlConnection("Server=studmysql01.fhict.local;Uid=dbi425113;Database=dbi425113;Pwd=bropro12;");
+            string sql = "SELECT count(id) FROM user;"; 
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            conn.Open();
+
+            // Send the query to the database and do something awesome with it
+
+            Object result = cmd.ExecuteScalar();
+            int count = -1; if (result != null) { count = Convert.ToInt32(result); }
+            MessageBox.Show(count.ToString());
+
+            conn.Close();
+
 
         }
     }
---
+++ WindowsFormsApp1/WindowsFormsApp1/Worker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Worker
+    {
+        private decimal hourlyWage;
+        
+        public double TotalHoursWorked()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
