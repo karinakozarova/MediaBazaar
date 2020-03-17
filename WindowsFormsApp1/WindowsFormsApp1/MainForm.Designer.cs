ba5a457a67df4eceba31f4ba59fbd97d976f2e0f
Evgeniy Terziev <i425986@fhict.nl>
2020-03-17 23:02:14 +0100

    Added checking for role and changing some controls

--- WindowsFormsApp1/WindowsFormsApp1/MainForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.Designer.cs
@@ -30,16 +30,18 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.employeesTab = new System.Windows.Forms.TabPage();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSearchEmployee = new System.Windows.Forms.Button();
+            this.btnFireManager = new System.Windows.Forms.Button();
+            this.btnRemoveEmployee = new System.Windows.Forms.Button();
+            this.btnCreateAdminManager = new System.Windows.Forms.Button();
             this.stocksTab = new System.Windows.Forms.TabPage();
             this.button3 = new System.Windows.Forms.Button();
             this.createStockBttn = new System.Windows.Forms.Button();
             this.departmentsTab = new System.Windows.Forms.TabPage();
             this.flpDepartments = new System.Windows.Forms.FlowLayoutPanel();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnFireEmployeeRequest = new System.Windows.Forms.Button();
+            this.btnHireEmployeeRequest = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.employeesTab.SuspendLayout();
             this.stocksTab.SuspendLayout();
@@ -55,77 +57,79 @@
             this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1284, 615);
+            this.tabControl1.Size = new System.Drawing.Size(1141, 492);
             this.tabControl1.TabIndex = 0;
             // 
             // employeesTab
             // 
-            this.employeesTab.Controls.Add(this.button6);
-            this.employeesTab.Controls.Add(this.button5);
-            this.employeesTab.Controls.Add(this.button4);
-            this.employeesTab.Controls.Add(this.button1);
-            this.employeesTab.Location = new System.Drawing.Point(4, 29);
+            this.employeesTab.Controls.Add(this.btnHireEmployeeRequest);
+            this.employeesTab.Controls.Add(this.btnFireEmployeeRequest);
+            this.employeesTab.Controls.Add(this.btnSearchEmployee);
+            this.employeesTab.Controls.Add(this.btnFireManager);
+            this.employeesTab.Controls.Add(this.btnRemoveEmployee);
+            this.employeesTab.Controls.Add(this.btnCreateAdminManager);
+            this.employeesTab.Location = new System.Drawing.Point(4, 25);
             this.employeesTab.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.employeesTab.Name = "employeesTab";
             this.employeesTab.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.employeesTab.Size = new System.Drawing.Size(1276, 582);
+            this.employeesTab.Size = new System.Drawing.Size(1133, 463);
             this.employeesTab.TabIndex = 0;
             this.employeesTab.Text = "Employees";
             this.employeesTab.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnSearchEmployee
             // 
-            this.button6.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(35, 390);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(1151, 65);
-            this.button6.TabIndex = 30;
-            this.button6.Text = "Search for employee";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnSearchEmployee.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchEmployee.Location = new System.Drawing.Point(31, 312);
+            this.btnSearchEmployee.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSearchEmployee.Name = "btnSearchEmployee";
+            this.btnSearchEmployee.Size = new System.Drawing.Size(1023, 52);
+            this.btnSearchEmployee.TabIndex = 30;
+            this.btnSearchEmployee.Text = "Search for employee";
+            this.btnSearchEmployee.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnFireManager
             // 
-            this.button5.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(35, 276);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(1151, 65);
-            this.button5.TabIndex = 29;
-            this.button5.Text = "Fire employee";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnFireManager.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFireManager.Location = new System.Drawing.Point(31, 221);
+            this.btnFireManager.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFireManager.Name = "btnFireManager";
+            this.btnFireManager.Size = new System.Drawing.Size(1023, 52);
+            this.btnFireManager.TabIndex = 29;
+            this.btnFireManager.Text = "Fire manager";
+            this.btnFireManager.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnRemoveEmployee
             // 
-            this.button4.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(35, 160);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(1151, 65);
-            this.button4.TabIndex = 28;
-            this.button4.Text = "Remove employee from the system";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnRemoveEmployee.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveEmployee.Location = new System.Drawing.Point(31, 128);
+            this.btnRemoveEmployee.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRemoveEmployee.Name = "btnRemoveEmployee";
+            this.btnRemoveEmployee.Size = new System.Drawing.Size(1023, 52);
+            this.btnRemoveEmployee.TabIndex = 28;
+            this.btnRemoveEmployee.Text = "Remove employee from the system";
+            this.btnRemoveEmployee.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnCreateAdminManager
             // 
-            this.button1.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(35, 46);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(1151, 65);
-            this.button1.TabIndex = 27;
-            this.button1.Text = "Create account";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCreateAdminManager.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateAdminManager.Location = new System.Drawing.Point(31, 37);
+            this.btnCreateAdminManager.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCreateAdminManager.Name = "btnCreateAdminManager";
+            this.btnCreateAdminManager.Size = new System.Drawing.Size(1023, 52);
+            this.btnCreateAdminManager.TabIndex = 27;
+            this.btnCreateAdminManager.Text = "Create admin/manager";
+            this.btnCreateAdminManager.UseVisualStyleBackColor = true;
             // 
             // stocksTab
             // 
             this.stocksTab.Controls.Add(this.button3);
             this.stocksTab.Controls.Add(this.createStockBttn);
-            this.stocksTab.Location = new System.Drawing.Point(4, 29);
+            this.stocksTab.Location = new System.Drawing.Point(4, 25);
             this.stocksTab.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.stocksTab.Name = "stocksTab";
             this.stocksTab.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.stocksTab.Size = new System.Drawing.Size(1276, 582);
+            this.stocksTab.Size = new System.Drawing.Size(1133, 463);
             this.stocksTab.TabIndex = 1;
             this.stocksTab.Text = "Stocks";
             this.stocksTab.UseVisualStyleBackColor = true;
@@ -133,10 +137,10 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(46, 136);
+            this.button3.Location = new System.Drawing.Point(41, 109);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(1151, 65);
+            this.button3.Size = new System.Drawing.Size(1023, 52);
             this.button3.TabIndex = 28;
             this.button3.Text = "View upcoming shelf requests";
             this.button3.UseVisualStyleBackColor = true;
@@ -145,10 +149,10 @@
             // createStockBttn
             // 
             this.createStockBttn.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.createStockBttn.Location = new System.Drawing.Point(46, 28);
+            this.createStockBttn.Location = new System.Drawing.Point(41, 22);
             this.createStockBttn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.createStockBttn.Name = "createStockBttn";
-            this.createStockBttn.Size = new System.Drawing.Size(1151, 65);
+            this.createStockBttn.Size = new System.Drawing.Size(1023, 52);
             this.createStockBttn.TabIndex = 27;
             this.createStockBttn.Text = "Create stock";
             this.createStockBttn.UseVisualStyleBackColor = true;
@@ -158,10 +162,10 @@
             // 
             this.departmentsTab.Controls.Add(this.flpDepartments);
             this.departmentsTab.Controls.Add(this.label4);
-            this.departmentsTab.Location = new System.Drawing.Point(4, 29);
+            this.departmentsTab.Location = new System.Drawing.Point(4, 25);
             this.departmentsTab.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.departmentsTab.Name = "departmentsTab";
-            this.departmentsTab.Size = new System.Drawing.Size(1276, 582);
+            this.departmentsTab.Size = new System.Drawing.Size(1133, 463);
             this.departmentsTab.TabIndex = 2;
             this.departmentsTab.Text = "Departments";
             this.departmentsTab.UseVisualStyleBackColor = true;
@@ -170,27 +174,48 @@
             // 
             this.flpDepartments.AutoScroll = true;
             this.flpDepartments.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.flpDepartments.Location = new System.Drawing.Point(40, 106);
-            this.flpDepartments.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.flpDepartments.Location = new System.Drawing.Point(36, 85);
             this.flpDepartments.Name = "flpDepartments";
-            this.flpDepartments.Size = new System.Drawing.Size(1206, 435);
+            this.flpDepartments.Size = new System.Drawing.Size(1072, 348);
             this.flpDepartments.TabIndex = 13;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(34, 35);
+            this.label4.Location = new System.Drawing.Point(30, 28);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(221, 37);
+            this.label4.Size = new System.Drawing.Size(190, 31);
             this.label4.TabIndex = 12;
             this.label4.Text = "Departments:";
             // 
+            // btnFireEmployeeRequest
+            // 
+            this.btnFireEmployeeRequest.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFireEmployeeRequest.Location = new System.Drawing.Point(31, 221);
+            this.btnFireEmployeeRequest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFireEmployeeRequest.Name = "btnFireEmployeeRequest";
+            this.btnFireEmployeeRequest.Size = new System.Drawing.Size(1023, 52);
+            this.btnFireEmployeeRequest.TabIndex = 31;
+            this.btnFireEmployeeRequest.Text = "Fire employee request";
+            this.btnFireEmployeeRequest.UseVisualStyleBackColor = true;
+            // 
+            // btnHireEmployeeRequest
+            // 
+            this.btnHireEmployeeRequest.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHireEmployeeRequest.Location = new System.Drawing.Point(31, 37);
+            this.btnHireEmployeeRequest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnHireEmployeeRequest.Name = "btnHireEmployeeRequest";
+            this.btnHireEmployeeRequest.Size = new System.Drawing.Size(1023, 52);
+            this.btnHireEmployeeRequest.TabIndex = 32;
+            this.btnHireEmployeeRequest.Text = "Hire employee request";
+            this.btnHireEmployeeRequest.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1286, 630);
+            this.ClientSize = new System.Drawing.Size(1143, 504);
             this.Controls.Add(this.tabControl1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
@@ -211,12 +236,14 @@
         private System.Windows.Forms.TabPage stocksTab;
         private System.Windows.Forms.TabPage departmentsTab;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCreateAdminManager;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button createStockBttn;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnSearchEmployee;
+        private System.Windows.Forms.Button btnFireManager;
+        private System.Windows.Forms.Button btnRemoveEmployee;
         private System.Windows.Forms.FlowLayoutPanel flpDepartments;
+        private System.Windows.Forms.Button btnFireEmployeeRequest;
+        private System.Windows.Forms.Button btnHireEmployeeRequest;
     }
 }
