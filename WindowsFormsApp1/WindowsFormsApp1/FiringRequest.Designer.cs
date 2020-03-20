0729d43a0fcc5f573f9da5c899f5633c323e687d
Evgeniy Terziev <i425986@fhict.nl>
2020-03-20 11:41:47 +0100

    Added logic for Adding and declining firing requests!

--- WindowsFormsApp1/WindowsFormsApp1/FiringRequest.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/FiringRequest.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.label6 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbDepartment = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tbxLastName = new System.Windows.Forms.TextBox();
+            this.tbxFirstName = new System.Windows.Forms.TextBox();
+            this.btnSendFiringRequest = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rtbReason = new System.Windows.Forms.RichTextBox();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label6
@@ -48,91 +49,103 @@
             this.label6.TabIndex = 87;
             this.label6.Text = "Select department:";
             // 
-            // comboBox1
+            // cmbDepartment
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(24, 71);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(228, 41);
-            this.comboBox1.TabIndex = 86;
-            this.comboBox1.Text = "Department";
+            this.cmbDepartment.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDepartment.FormattingEnabled = true;
+            this.cmbDepartment.Location = new System.Drawing.Point(24, 71);
+            this.cmbDepartment.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbDepartment.Name = "cmbDepartment";
+            this.cmbDepartment.Size = new System.Drawing.Size(228, 41);
+            this.cmbDepartment.TabIndex = 86;
+            this.cmbDepartment.Text = "Department";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(19, 146);
+            this.label3.Location = new System.Drawing.Point(20, 126);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(179, 25);
             this.label3.TabIndex = 85;
             this.label3.Text = "Select employee:";
             // 
-            // textBox2
+            // tbxLastName
             // 
-            this.textBox2.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(24, 245);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(228, 40);
-            this.textBox2.TabIndex = 84;
-            this.textBox2.Text = "Last name";
+            this.tbxLastName.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxLastName.Location = new System.Drawing.Point(24, 271);
+            this.tbxLastName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbxLastName.Name = "tbxLastName";
+            this.tbxLastName.Size = new System.Drawing.Size(228, 40);
+            this.tbxLastName.TabIndex = 84;
+            this.tbxLastName.Text = "Last name";
             // 
-            // textBox1
+            // tbxFirstName
             // 
-            this.textBox1.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(24, 189);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(228, 40);
-            this.textBox1.TabIndex = 83;
-            this.textBox1.Text = "First name";
+            this.tbxFirstName.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxFirstName.Location = new System.Drawing.Point(24, 218);
+            this.tbxFirstName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbxFirstName.Name = "tbxFirstName";
+            this.tbxFirstName.Size = new System.Drawing.Size(228, 40);
+            this.tbxFirstName.TabIndex = 83;
+            this.tbxFirstName.Text = "First name";
             // 
-            // button1
+            // btnSendFiringRequest
             // 
-            this.button1.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(203, 317);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(239, 46);
-            this.button1.TabIndex = 82;
-            this.button1.Text = "Send request";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSendFiringRequest.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendFiringRequest.Location = new System.Drawing.Point(202, 324);
+            this.btnSendFiringRequest.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSendFiringRequest.Name = "btnSendFiringRequest";
+            this.btnSendFiringRequest.Size = new System.Drawing.Size(239, 46);
+            this.btnSendFiringRequest.TabIndex = 82;
+            this.btnSendFiringRequest.Text = "Send request";
+            this.btnSendFiringRequest.UseVisualStyleBackColor = true;
+            this.btnSendFiringRequest.Click += new System.EventHandler(this.BtnSendFiringRequest_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(324, 28);
+            this.label1.Location = new System.Drawing.Point(309, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(169, 25);
             this.label1.TabIndex = 81;
             this.label1.Text = "Reason for firing";
             // 
-            // richTextBox1
+            // rtbReason
             // 
-            this.richTextBox1.Font = new System.Drawing.Font("Montserrat", 16F);
-            this.richTextBox1.Location = new System.Drawing.Point(314, 71);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(359, 214);
-            this.richTextBox1.TabIndex = 80;
-            this.richTextBox1.Text = "Reason for firing";
+            this.rtbReason.Font = new System.Drawing.Font("Montserrat", 16F);
+            this.rtbReason.Location = new System.Drawing.Point(314, 71);
+            this.rtbReason.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rtbReason.Name = "rtbReason";
+            this.rtbReason.Size = new System.Drawing.Size(359, 240);
+            this.rtbReason.TabIndex = 80;
+            this.rtbReason.Text = "Reason for firing";
+            // 
+            // tbxUsername
+            // 
+            this.tbxUsername.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxUsername.Location = new System.Drawing.Point(24, 163);
+            this.tbxUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(228, 40);
+            this.tbxUsername.TabIndex = 88;
+            this.tbxUsername.Text = "Username";
             // 
             // FiringRequest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(700, 381);
+            this.Controls.Add(this.tbxUsername);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmbDepartment);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxLastName);
+            this.Controls.Add(this.tbxFirstName);
+            this.Controls.Add(this.btnSendFiringRequest);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.rtbReason);
             this.Name = "FiringRequest";
             this.Text = "FiringRequest";
             this.ResumeLayout(false);
@@ -143,12 +156,13 @@
         #endregion
 
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbDepartment;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbxLastName;
+        private System.Windows.Forms.TextBox tbxFirstName;
+        private System.Windows.Forms.Button btnSendFiringRequest;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtbReason;
+        private System.Windows.Forms.TextBox tbxUsername;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/RequestControl.cs
+++ WindowsFormsApp1/WindowsFormsApp1/RequestControl.cs
@@ -12,9 +12,51 @@ namespace WindowsFormsApp1
 {
     public partial class RequestControl : UserControl
     {
-        public RequestControl()
+        private int personId;
+        private int createdById;
+        private int departmentId;
+        private string username;
+        private string firstName;
+        private string lastName;
+        private string description;
+        ApproveManagerRequests form;
+        public RequestControl(int personId, int createdById, int departmentId, string description, string firstName, string lastName, string username, ApproveManagerRequests form = null)
         {
             InitializeComponent();
+            lblRequest.Text = $"{firstName} {lastName} firing!";
+            lblRequestDescription.Text = description;
+            this.personId = personId;
+            this.createdById = createdById;
+            this.departmentId = departmentId;
+            this.username = username;
+            this.description = description;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.form = form;
+        }
+
+        private void BtnDetails_Click(object sender, EventArgs e)
+        {
+            bool flag = true;
+            FiringRequest frForm = new FiringRequest(flag, this.departmentId, this.username, this.firstName, this.lastName, this.description);
+            frForm.Show();
+        }
+
+        private void BtnApprove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDecline_Click(object sender, EventArgs e)
+        {
+            foreach (FiringRequests fr in FiringRequests.GetAllFiringRequests())
+            {
+                if (fr.PersonId == personId)
+                {
+                    fr.DeclineFiringRequest();
+                }
+            }
+            form.UpdateGUI();
         }
     }
 }
